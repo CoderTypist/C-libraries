a3d01e982322ff1bdd38929b365e57b36b2e54f3
CoderTypist <39784989+CoderTypist@users.noreply.github.com>
2018-10-18 14:21:17 -0500

    Update cjbfile.h
    I put the wrong code here. The file now has the right code.

--- cjbfile.h
+++ cjbfile.h
@@ -1,42 +1,39 @@
-#ifndef _cjbstring_h_
-#define _cjbstring_h_
+#ifndef _cjbfile_h_
+#define _cjbfile_h_
 
 /* Written by Christian Bargraser October 2018 */
 
-/* Returns a String that contains the received String and has the char
- * Appended to the end
- * This function can receive a char* that has not had memory dynamically
- * allocated for it
+/* Returns the next character in the file that is not '\n' */
+char GetRealC(FILE *fPointer);
+
+/* Returns the next character in the file
+ * The calling function will not perceive any change in the file pointer
  */
-char* AppendChar(char *original, char c);
+char SeeNextChar(FILE *fPointer);
 
-/* char* original must have dynamically allocated memory
- * The program will crash if the String was not dynamically allocated
- * Returns -1 if original is NULL
- * You must use AppendChar if the String was not dynamically allocated
- * You should use AppendChar() if there exists the possibility that the 
- * received String was not dynamically allocated*/
- 
-int AppendCharToMallocd(char* original, char c);
-
-/* Returns a String that starts at startIndex
- * The returned String ends at and includes endIndex
+/* Returns the next character in the file that is not '\n
+ * The calling function will not perceive any change in the file pointer
  */
-char* Substring(char *original, int startIndex, int endIndex);
+char SeeNextRealC(FILE *fPointer);
 
-/* A String containing the char is returned */
-char* CharToString(char c);
-
-/* Receives a pointer and frees the memory that it is pointing to
- * However, if the pointer is pointing to a structure, and the structure
- * Has attributes that are pointers, the memory that those pointers point
- * to will not be freed
- * Call this method as follows:
- * nfree((void*)&variable);
+/* Returns a String that contains the following letters in a file 
+ * If EOF is reached before constructing a String of size numLetter,
+ * the partial String will be returned. It might be necessary to check
+ * the size of the returned String in the calling function
  */
-void nfree(void **dp);
+char* SeeNextLetters(FILE *fPointer, int numLetters);
 
-/* Thre received String is freed and set equal to NULL */
-void StringFree(char **dp);
+/* Moves the file pointer to the location specified by marker
+ * All this method does is call fseek
+ */
+int GoTo(FILE *fPointer, int marker);
+
+/* Moves the file pointer forward by the specified number of characters */
+int Advance(FILE *fPointer, int numLetters);
+
+/* Moves the file pointer forward by the specified number of characters
+ * However, '\n' is not counted
+ */
+int RealAdvance(FILE *fPointer, int numLetters);
 
 #endif
