d6faaa0dd512956d240c82a41902ffd38cb80e11
CoderTypist <39784989+CoderTypist@users.noreply.github.com>
2019-01-09 21:00:20 -0600

    Create cjbbyte.h
    Function prototypes were added

---
+++ cjbbyte.h
@@ -0,0 +1,28 @@
+#ifndef _cjbbyte_h_
+#define _cjbbyte_h_
+
+/* Returns the desired bit from the char
+ * The char is representative of a byte
+ */
+int bitFromChar(char c, int index);
+
+/* Prints the bits that make up the char
+ */
+void printCharBits(char c);
+
+/* Prints all of the bits that make up a String
+ * EOF is omitted when printing the bits
+ * There is a space between every byte/8 bits
+ * Printing continues on a new line after
+ * bytesPerLine bytes have been printed
+ */
+void printStringBits(char *cp, int bytesPerLine);
+
+/* Prints all the bits within the specified number of bytes
+ * Example: Can be used to print all of the bits within a struct
+ * Printing continues on a new line after
+ * bytesPerLine bytes have been printed
+ */
+void printnbytes(void *vp, int bytes, int bytesPerLine);
+
+#endif
