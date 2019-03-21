d62ab4e35a8096d764c5da085a9d0d27a8045462
CoderTypist <39784989+CoderTypist@users.noreply.github.com>
2019-03-21 16:43:12 -0500

    Update cjbbyte.h
    Added the function prototypes for memPrint(), memBits(), and memHex()

--- cjbbyte.h
+++ cjbbyte.h
@@ -31,6 +31,24 @@ void printnbytes(void *vp, int bytes, int bytesPerLine);
 
 /* returns a char* that has the bits in the specified size of memory
  */
-char* getnbytes(void *vp, int bytes)
+char* getnbytes(void *vp, int bytes);
+
+/* memPrint does the same thing as printnbytes, except that it accepts 
+ * more parameters. If bool addressColumn is true, the address of the first
+ * byte in the printed row will be printed. If bool asBits is true, then 
+ * the bits of the bytes will be printed. If bool asBits is false, then the hex
+ * value of the byte will be printed.
+ */
+void memPrint(void *vp, int bytes, int bytesPerLine, bool addressColumn, bool asBits);
+
+/* Calls memPrint. Sends true for bool addressColumn.
+ * Sends true for bool asBits to print the bits in the bytes.
+ */
+void memBits(void *vp, int bytes, int bytesPerLine);
+
+/* Calls memPrint. Sends true for bool addressColumn.
+ * Sends false for bool asBits to prints the hex value of the bytes.
+ */
+void memHex(void *vp, int bytes, int bytesPerLine);
 
 #endif
